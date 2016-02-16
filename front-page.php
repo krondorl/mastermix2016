e892d6e2183ccc83180b56650a888bdf5eb4f102
Adam Burucs <adam.burucs@gmail.com>
2016-02-16 19:43:14 +0100

    Init commit
    Beginning of the template, work in progress

---
+++ front-page.php
@@ -0,0 +1,115 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	
+	 <h2>Könyv</h2>
+
+    <div class="clearfix">
+      <div class="col-md-4">
+        <a class="" href="https://www.dropbox.com/s/3ek9l2hptexql0m/burucs_adam-audio_mastering_es_mixing_2015-10-30-c.zip?dl=0" target="_blank"><img class="book" src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/mastering-mixing-konyv-borito.jpg" alt="Audio mastering és mixing könyv borító"></a>
+      </div>
+      <div class="col-md-8">
+        <p>A jó zene megírása és jó rögzítése után a legfontosabb fázis a mixing, azaz keverés. A mixing során beállítjuk a hangerőt, a panorámát, a dinamikát, a frekvencia eloszlást és kreatívan effektezhetünk is. Olyan szakma, ami egyszerre tudomány és művészet.</p>
+        <p>Hiába hallottunk a sokak által misztikusnak tartott masteringről, ha előtte a mixünk nincs rendben, akkor azon a mastering nem fog segíteni. Ha pedig a mixing nem jó, akkor ott kell először rendet tenni.</p>
+        <p>Ebben a könyvben elsősorban a kezdő zeneszerzőknek mutatok eszközöket az otthoni sztereó mixingben, majd utána a masteringben is.</p>
+        <a class="download" href="https://www.dropbox.com/s/3ek9l2hptexql0m/burucs_adam-audio_mastering_es_mixing_2015-10-30-c.zip?dl=0" target="_blank">Ingyenes letöltés</a>
+      </div>
+    </div>
+
+
+   <h2>Tartalomjegyzék</h2>
+   
+   <div class="clearfix">
+    <ul class="col-md-6">
+      <li>Bevezetés</li>
+      <li>Hardver</li>
+      <li>Szoftver</li>
+      <li>Előkészületek</li>
+      <li>Hangerő és panoráma (volume, panning)</li>
+      <li>Dinamika (compressor, limiter, gate, expander, multi band compressor, de-esser)</li>
+      <li>Hangszínszabályzás (EQ, equalizer)</li>
+      <li>Visszhang (delay)</li>
+      <li>Zengető (reverb)</li>
+      <li>Modulációs effektek (phaser, flanger, chorus)</li>
+      <li>Torzító (overdrive, distortion, fuzz)</li>
+      <li>Szaturáció (saturation)</li>
+      <li>Sztereó vastagítás (stereo widening)</li>
+      <li>Fázis (phase)</li>
+      <li>Tranziensek (transient)</li>
+      <li>Mid/side feldolgozás</li>
+      <li>Sidechain működés</li>
+      <li>Automatizálás</li>
+      <li>Vizuális eszközök</li>
+      <li>Audio formátumok</li>
+      <li>Emberi tényezők a keverés során: érzelmek, hallás, látás</li>
+      <li>Stílusoktól független keverés</li>
+      <li>Korrektív és kreatív folyamat</li>
+      <li>Referencia zenék használata</li>
+      <li>Imitáció</li>
+    </ul>
+    
+    <ul class="col-md-6">
+      <li>Első mixing példa</li>
+      <li>Második mixing példa</li>
+      <li>Harmadik mixing példa</li>
+      <li>Negyedik mixing példa</li>
+      <li>Ötödik mixing példa</li>
+      <li>Hatodik mixing példa</li>
+      <li>CPU erőforrás menedzsment</li>
+      <li>Időmenedzsment</li>
+      <li>Együttműködés a mastering hangmérnökkel</li>
+      <li>Mastering - bevezetés</li>
+      <li>Mastering – dinamika</li>
+      <li>Mastering – EQ</li>
+      <li>Mastering – sztereó kép</li>
+      <li>Mastering – hangerő, limiter</li>
+      <li>Mastering – effektlánc</li>
+      <li>Mastering – munkafolyamat</li>
+      <li>Mastering példa</li>
+      <li>Hozzáállás és fejlődés</li>
+      <li>Művelődjünk! Válogatott zenék meghallgatása több stílusban</li>
+      <li>További tananyagok</li>
+      <li>Mixing ellenőrzési lista</li>
+      <li>Mastering ellenőrzési lista</li>
+      <li>Elérhetőség</li>
+
+    </ul>
+     
+   </div>
+
+	
+	 <h2 class="about">Rólunk</h2>
+	<div class="clearfix">
+      <div class="col-md-4">
+        <h3>Burucs Ádám</h3>
+        <h4>alapító, szerző, mixing, mastering, zene</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro qui saepe accusamus recusandae possimus, vitae adipisci eius aut commodi ipsum voluptatum impedit. Itaque, nobis eveniet sunt dolorum laudantium vitae earum!</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Szigetvári Zsolt</h3>
+        <h4>szakmai lektor, zenész</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quod laudantium necessitatibus culpa molestiae deserunt totam fugiat dolorem nobis nam aspernatur, magni quas mollitia molestias, doloremque minus saepe vitae magnam.</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Dobóczi Attila</h3>
+        <h4>szakmai lektor, zenész</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus optio non sequi libero nesciunt aliquam autem facilis ab cupiditate rerum sapiente, consectetur similique nam blanditiis, repellendus fugit, ullam, id placeat.</p>
+      </div>
+	  
+	</div>
+	
+	<h2>Kapcsolat</h2>
+	<p>
+      <a href="https://www.facebook.com/groups/audiomasteringesmixing/" target="_blank">Csatlakozz a FaceBook csoporthoz!</a><br>
+      <a href="https://www.facebook.com/afmastering/" target="_blank">Kérj ajánlatot mixing és mastering szolgáltatásra!</a>
+	</p>
+
+	<div>
+	  <?php echo do_shortcode('[contact-form-7 id="10" title="Contact form 1"]'); ?>
+	</div>
+			
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Masteringmixing.hu
+## Wordpress template
+
+This child template is based on the standard twentysixteen theme.
+
+Live site is at: www.masteringmixing.hu
