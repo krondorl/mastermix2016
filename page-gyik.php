d98cfc4600f1564584f71ac9c11bbe57899fbba2
Adam Burucs <adam.burucs@gmail.com>
2016-02-17 10:00:02 +0100

    GYIK hozzaadas

---
+++ page-gyik.php
@@ -0,0 +1,94 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	
+	 <h2>GYIK / Gyakori kérdések</h2>
+
+    <p>Az alábbiakban összeírtam néhány választ kezdőknek, hogy egyszerűbb legyen dönteni.</p>
+   
+    <h3>Milyen kezdő hangkártyát vegyek?</h3>
+
+    <ul>
+      <li><a href="http://hitspace.hu/steinberg-ur-22-mk2.html" target="_blank">Steinberg UR22</a></li>
+      <li><a href="http://hitspace.hu/focusrite-scarlett-2i2-1.html" target="_blank">Focusrite 2i2</a></li>
+    </ul>
+   
+    <h3>Milyen kezdő monitor hangfalat vegyek?</h3>
+    
+    <ul>
+      <li><a href="http://hitspace.hu/tannoy-reveal-502.html" target="_blank">Tannoy REVEAL 502</a></li>
+      <li><a href="http://hitspace.hu/mackie-mr5-mk3.html" target="_blank">Mackie MR5 mk3</a></li>
+      <li><a href="http://hitspace.hu/yamaha-hs7.html" target="_blank">Yamaha HS7</a></li>
+    </ul>
+    
+    <h3>Milyen fejhallgatót vegyek?</h3>
+    
+    <ul>
+      <li><a href="http://hitspace.hu/audio-technica-ath-m50x.html" target="_blank">Audio-Technica ATH-M50X</a></li>
+      <li><a href="http://hitspace.hu/sony-mdr-7506.html" target="_blank">Sony MDR-7506</a></li>
+      <li><a href="http://hitspace.hu/beyerdynamic-dt-770-pro-80-ohm.html" target="_blank">Beyerdynamic DT-770 Pro 80 Ohm</a></li>
+      <li><a href="http://hitspace.hu/beyerdynamic-dt-990-pro.html" target="_blank">Beyerdynamic DT-990 Pro</a></li>
+    </ul>
+    
+    <h3>Az egyik cucc 20 Hz - 20 kHz-et tud, akkor az jó, ugye?</h3>
+    
+    <p>Nem. A legtöbb ilyen sajnos marketing duma.
+    Csak akkor lehet elhinni,
+    ha megvan adva vizuális grafikon,
+    vagy feltűntetve a hibahatár.
+    Pl. ha a 20 Hz-et -12 dB hangerővel viszi át, abból nem sokat fogsz hallani.</p>
+    
+    <h3>Az egyik hangfal 1000 Watt-ot tud, akkor az jó hangos, ugye?</h3>
+    
+    <p>Nem. Mert nem a Watt-beli teljesítmény számít, hanem a hangerő érzékenység, ami decibelben van megadva jobb helyeken.</p>
+    
+    <h3>Milyen olcsó fülhallgatót vegyek?</h3>
+
+    <p>
+      <a href="http://www.arukereso.hu/fulhallgato-fejhallgato-c3109/sennheiser/mx-170-p44071477/">Sennheiser MX 170</a>
+    </p>
+   
+    <h3>Milyen studio programot / DAW használjak kimondottan mixingre és masteringre?</h3>
+
+      <ul>
+        <li><a href="https://www.ableton.com/en/live/">Ableton Live</a></li>
+        <li><a href="http://www.reaper.fm/">Reaper</a></li>
+        <li><a href="http://www.avid.com/US/products/pro-tools-software">Pro Tools</a></li>
+        <li><a href="http://www.apple.com/logic-pro/">Logic Pro X</a></li>
+        <li><a href="http://www.steinberg.net/en/products/cubase/start.html">Cubase</a></li>
+      </ul>
+
+    <h3>Milyen studio programot / DAW használjak kimondottan zeneszerzésre?</h3>
+
+    <p>Amilyet szeretnél! A hangzás a használt hangmintákon + plugineken fog múlni, és nem a studio programon.</p>
+
+     <h3>Milyen plugineket használjak mixingre és masteringre?</h3>
+
+      <ul>
+        <li><a href="http://www.fabfilter.com/" target="_blank">Fabfilter</a></li>
+        <li><a href="http://www.waves.com/" target="_blank">Waves</a></li>
+        <li><a href="http://www.uaudio.com/" target="_blank">Universal Audio</a></li>
+      </ul>
+
+    <h3>Milyen olcsó pluginek vannak?</h3>
+
+    <p>
+      <a href="http://www.musicradar.com/computermusic/cm-plugins-full-list-of-our-exclusive-free-vst-au-plugin-instruments-and-effects-571808" target="_blank">CM studio plugins (a magazin mellé jár kb 2000 dollár értékű plugin)</a>
+    </p>
+
+    <h3>Ingyenes pluginek</h3>
+    <ul>
+      <li>
+        <a href="http://bedroomproducersblog.com/free-vst-plugins/" target="_blank">Bedroom Producer Blog</a>
+      </li>
+      <li>
+        <a href="https://sites.google.com/site/kvrosc/recommended-plugins" target="_blank">KVR OSC plugins</a>
+      </li>
+    </ul>
+
+
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
