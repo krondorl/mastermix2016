0cfc92007132ffc37737989915c5868615f7cf4f
Adam Burucs <adam.burucs@gmail.com>
2016-02-18 13:52:38 +0100

    FB OG kep hozzadasa

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/mastermix-fb-opengraph-image.jpg" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
