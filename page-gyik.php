4fd21e049640606564e525959e2d876010b7a098
Adam Burucs <adam.burucs@gmail.com>
2016-02-18 09:19:55 +0100

    GYIK update kepekkel

--- page-gyik.php
+++ page-gyik.php
@@ -7,46 +7,87 @@
 
     <p>Az alábbiakban összeírtam néhány választ kezdőknek, hogy egyszerűbb legyen dönteni.</p>
    
-    <h3>Milyen számítógépet vegyek zenei célokra?</h3>
-   <ul>
-    <li>
-      <a href="https://www.scan.co.uk/3xs/custom/daw-digital-audio-workstation-pcs/workstations#anc" target="_blank">PC - Scan.co.uk - Pro Audio Workstations</a>
-    </li>
-    <li>
-      <a href="http://www.istyle.eu/hu/mac/mac-mini/mac-mini-dual-core-i5-2-8ghz-2014.html" target="_blank">MAC - Istyle</a>
-    </li>
-   </ul>
-    
-    <h3>Milyen kezdő hangkártyát vegyek?</h3>
-
-    <ul>
-      <li><a href="http://hitspace.hu/steinberg-ur-22-mk2.html" target="_blank">Steinberg UR22</a></li>
-      <li><a href="http://hitspace.hu/focusrite-scarlett-2i2-1.html" target="_blank">Focusrite 2i2</a></li>
-    </ul>
+   <div>
+     <div class="col-md-8 nopadding">
+        <h3>Milyen számítógépet vegyek zenei célokra?</h3>
+       <ul>
+        <li>
+          <a href="https://www.scan.co.uk/3xs/custom/daw-digital-audio-workstation-pcs/workstations#anc" target="_blank">PC - Scan.co.uk - Pro Audio Workstations</a>
+        </li>
+        <li>
+          <a href="http://www.istyle.eu/hu/mac/mac-mini/mac-mini-dual-core-i5-2-8ghz-2014.html" target="_blank">MAC - Istyle</a>
+        </li>
+       </ul>
+     </div>
+     <div class="col-md-4 nopadding">
+       <img src="https://www.scan.co.uk/images/3xs/configurations/config/10181.png" alt="">
+     </div>
+   </div>
    
-    <h3>Milyen kezdő monitor hangfalat vegyek?</h3>
+   
+    <div>
+      <div class="col-md-8 nopadding">
+        <h3>Milyen kezdő hangkártyát vegyek?</h3>
+
+        <ul>
+          <li><a href="http://hitspace.hu/steinberg-ur-22-mk2.html" target="_blank">Steinberg UR22</a></li>
+          <li><a href="http://hitspace.hu/focusrite-scarlett-2i2-1.html" target="_blank">Focusrite 2i2</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="https://www.steinberg.net/index.php?eID=tx_nawsecuredl&u=0&g=0&t=1455873253&hash=b3af8ed8088d3d575935998cedfee3bd86eebadc&file=uploads/RTEmagicC_steinberg_ur22_front_mirror_29.png.png" alt="">
+      </div>
+    </div>
+      
+   
+   <div>
+     <div class="col-md-8 nopadding">
+        <h3>Milyen kezdő monitor hangfalat vegyek?</h3>
+
+        <ul>
+          <li><a href="http://hitspace.hu/tannoy-reveal-502.html" target="_blank">Tannoy REVEAL 502</a></li>
+          <li><a href="http://hitspace.hu/mackie-mr5-mk3.html" target="_blank">Mackie MR5 mk3</a></li>
+          <li><a href="http://hitspace.hu/yamaha-hs7.html" target="_blank">Yamaha HS7</a></li>
+        </ul>
+     </div>
+     <div class="col-md-4 nopadding">
+       <img src="http://hitspace.hu/media/catalog/product/cache/1/image/2100x/6b9ffbf72458f4fd2d3cb995d92e8889/m/r/mrcomparsion.jpg" alt="">
+     </div>
+   </div>
+   
     
-    <ul>
-      <li><a href="http://hitspace.hu/tannoy-reveal-502.html" target="_blank">Tannoy REVEAL 502</a></li>
-      <li><a href="http://hitspace.hu/mackie-mr5-mk3.html" target="_blank">Mackie MR5 mk3</a></li>
-      <li><a href="http://hitspace.hu/yamaha-hs7.html" target="_blank">Yamaha HS7</a></li>
-    </ul>
+    <div>
+      <div class="col-md-8 nopadding">
+        <h3>Milyen akusztikai szivacsot vegyek a hangfal alá?</h3>
+
+        <p>
+          <a href="http://hitspace.hu/auralex-auralex-mopad-4-pack.html" target="_blank">Auralex MoPAD 4</a>
+        </p>
+        
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="http://hitspace.hu/media/catalog/product/cache/1/image/2100x/6b9ffbf72458f4fd2d3cb995d92e8889/a/u/auralex-mopad-monitor-isolation-pads.jpg" alt="">
+      </div>
+    </div>
     
-    <h3>Milyen akusztikai szivacsot vegyek a hangfal alá?</h3>
-    
-    <p>
-      <a href="http://hitspace.hu/auralex-auralex-mopad-4-pack.html" target="_blank">Auralex MoPAD 4</a>
-    </p>
-    
-    <h3>Milyen fejhallgatót vegyek?</h3>
-    
-    <ul>
-      <li><a href="http://hitspace.hu/audio-technica-ath-m50x.html" target="_blank">Audio-Technica ATH-M50X</a></li>
-      <li><a href="http://hitspace.hu/sony-mdr-7506.html" target="_blank">Sony MDR-7506</a></li>
-      <li><a href="http://hitspace.hu/beyerdynamic-dt-770-pro-80-ohm.html" target="_blank">Beyerdynamic DT-770 Pro 80 Ohm</a></li>
-      <li><a href="http://hitspace.hu/beyerdynamic-dt-990-pro.html" target="_blank">Beyerdynamic DT-990 Pro</a></li>
-      <li><a href="http://hitspace.hu/akg-k271-mkii-studio.html" target="_blank">AKG K271 MKII Studio</a></li>
-    </ul>
+
+    <div>
+      <div class="col-md-8 nopadding">
+        <h3>Milyen fejhallgatót vegyek?</h3>
+
+        <ul>
+          <li><a href="http://hitspace.hu/audio-technica-ath-m50x.html" target="_blank">Audio-Technica ATH-M50X</a></li>
+          <li><a href="http://hitspace.hu/sony-mdr-7506.html" target="_blank">Sony MDR-7506</a></li>
+          <li><a href="http://hitspace.hu/beyerdynamic-dt-770-pro-80-ohm.html" target="_blank">Beyerdynamic DT-770 Pro 80 Ohm</a></li>
+          <li><a href="http://hitspace.hu/beyerdynamic-dt-990-pro.html" target="_blank">Beyerdynamic DT-990 Pro</a></li>
+          <li><a href="http://hitspace.hu/akg-k271-mkii-studio.html" target="_blank">AKG K271 MKII Studio</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="http://hitspace.hu/media/catalog/product/cache/1/image/2100x/6b9ffbf72458f4fd2d3cb995d92e8889/d/t/dt770p_big.jpg" alt="">
+      </div>
+    </div>
+            
     
     <h3>Milyen kezdő mikrofont vegyek?</h3>
     
@@ -73,19 +114,27 @@
       <li><a href="http://hitspace.hu/miditech-midistart-music-25.html" target="_blank">Miditech MIDISTART MUSIC 25</a></li>
     </ul>
             
-    <h3>Az egyik cucc, pl. fül / fejhallgató 20 Hz - 20 kHz-et tud, akkor az jó, ugye?</h3>
-    
-    <p>Nem. A legtöbb ilyen sajnos marketing duma.
-    Általában akkor lehet elhinni,
-    ha megvan adva vizuális grafikon,
-    vagy feltűntetve a válaszérték.
-    Pl. ha a 20 Hz-et -50 dB hangerővel viszi át, abból nem sokat fogsz hallani :) Példának okáért nézd meg ezt a high end hangfalat.
-    Az <a href="http://www.thomann.de/gb/event_opal.htm" target="_blank">Event Opal hangfalnak</a> 35 Hz az alja és darabja több mint 500.000 Ft.</p>
-    
-    <p>Érdemes megnézni az Opal frekvencia válasz diagramját.</p>
-    
-    <img src="http://www.eventelectronics.com/images/opal-freq-response-large.gif" alt="Event Opal frekvencia válasz diagram">
-    
+            
+    <div>
+      <div class="col-md-8 nopadding">
+        <h3>Az egyik cucc, pl. fül / fejhallgató 20 Hz - 20 kHz-et tud, akkor az jó, ugye?</h3>
+
+        <p>Nem. A legtöbb ilyen sajnos marketing duma.
+        Általában akkor lehet elhinni,
+        ha megvan adva vizuális grafikon,
+        vagy feltűntetve a válaszérték.
+        Pl. ha a 20 Hz-et -50 dB hangerővel viszi át, abból nem sokat fogsz hallani :) Példának okáért nézd meg ezt a high end hangfalat.
+        Az <a href="http://www.thomann.de/gb/event_opal.htm" target="_blank">Event Opal hangfalnak</a> 35 Hz az alja és darabja több mint 500.000 Ft.</p>
+
+        <p>Érdemes megnézni az Opal frekvencia válasz diagramját.</p>
+        
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="http://www.eventelectronics.com/images/opal-freq-response-large.gif" alt="Event Opal frekvencia válasz diagram">
+        
+      </div>
+    </div>
+            
     <h3>Az egyik hangfal 1000 Watt-ot tud, akkor az jó hangos, ugye?</h3>
     
     <p>Nem. Mert nem a Watt-beli teljesítmény számít, hanem a hangerő érzékenység, ami decibelben van megadva jobb helyeken.</p>
@@ -100,15 +149,23 @@
         <li><a href="http://www.steinberg.net/en/products/cubase/start.html">Cubase</a></li>
       </ul>
 
-    <h3>Milyen studio programot / DAW használjak kimondottan zeneszerzésre?</h3>
+   <div>
+     <div class="col-md-8 nopadding">
+        <h3>Milyen studio programot / DAW használjak kimondottan zeneszerzésre?</h3>
 
-    <p>Amilyet szeretnél! A hangzás a használt hangmintákon, plugineken és mixingen fog múlni, és nem a studio programon.</p>
+        <p>Amilyet szeretnél! A hangzás a használt hangmintákon, plugineken és mixingen fog múlni, és nem a studio programon.</p>
 
-    <ul>
-      <li><a href="https://www.image-line.com/flstudio/">FL Studio</a></li>
-      <li><a href="https://www.propellerheads.se/reason">Reason</a></li>
-      <li><a href="http://studioone.presonus.com/">Studio One</a></li>
-    </ul>
+        <ul>
+          <li><a href="https://www.image-line.com/flstudio/">FL Studio</a></li>
+          <li><a href="https://www.propellerheads.se/reason">Reason</a></li>
+          <li><a href="http://studioone.presonus.com/">Studio One</a></li>
+        </ul>
+     </div>
+     <div class="col-md-4 nopadding">
+       <img src="https://www.image-line.com/flstudio/fl12full_1.jpg" alt="">
+     </div>
+   </div>
+   
     
      <h3>Milyen plugineket használjak mixingre és masteringre?</h3>
 
@@ -148,20 +205,35 @@
       <li><a href="https://www.youtube.com/channel/UCjRzsiP_aDWWLHV4-2LKBtg" target="_blank">Recording Revolution</a></li>
     </ul>
 
-   <h3>Milyen print és online magazinokat olvassak?</h3>
-         
-    <ul>
-      <li><a href="http://www.soundonsound.com/" target="_blank">Sound On Sound</a></li>
-      <li><a href="http://www.musicradar.com/computermusic" target="_blank">Computer Music</a></li>
-      <li><a href="http://www.musicradar.com/futuremusic" target="_blank">Future Music</a></li>
-    </ul>
+      <div>
+      <div class="col-md-8 nopadding">
+        <h3>Milyen print és online magazinokat olvassak?</h3>
+        <ul>
+          <li><a href="http://www.soundonsound.com/" target="_blank">Sound On Sound</a></li>
+          <li><a href="http://www.musicradar.com/computermusic" target="_blank">Computer Music</a></li>
+          <li><a href="http://www.musicradar.com/futuremusic" target="_blank">Future Music</a></li>
+        </ul>
+        
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="http://www.cylonix.com/images/sosCOVER_April2013.jpg" alt="SOS magazine">
+      </div>
+    </div>
+  
+    <div>
+      <div class="col-md-8 nopadding">
+        <h3>Milyen könyveket olvassak mixing és mastering témában?</h3>
+        <ul>
+          <li><a href="http://www.amazon.com/Mixing-Secrets-Small-Studio-Senior/dp/0240815807" target="_blank">Mixing Secrets for the Small Studio</a></li>
+          <li><a href="http://www.amazon.com/Mixing-Engineers-Handbook-Bobby-Owsinski/dp/128542087X/" target="_blank">The Mixing Engineer's Handbook</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4 nopadding">
+        <img src="http://ecx.images-amazon.com/images/I/51KqC7Iw-TL._SX391_BO1,204,203,200_.jpg" alt="Mixing secrets">
+      </div>
+    </div>
     
-    <h3>Milyen könyveket olvassak mixing és mastering témában?</h3>
                   
-    <ul>
-      <li><a href="http://www.amazon.com/Mixing-Secrets-Small-Studio-Senior/dp/0240815807" target="_blank">Mixing Secrets for the Small Studio</a></li>
-      <li><a href="http://www.amazon.com/Mixing-Engineers-Handbook-Bobby-Owsinski/dp/128542087X/" target="_blank">The Mixing Engineer's Handbook</a></li>
-    </ul>
                    
     </main><!-- .site-main -->
 </div><!-- .content-area -->
