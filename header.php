2ee6958a7d8b0d7941380dcac9a8934334c68945
Adam Burucs <adam.burucs@gmail.com>
2016-02-18 09:25:37 +0100

    Analytics hozzadas

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php include_once("analytics.php") ?>
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
