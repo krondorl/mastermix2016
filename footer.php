fbb8b59b632c71a9c07f9f42c894e2b0f75c0221
Adam Burucs <adam.burucs@gmail.com>
2016-02-18 12:11:12 +0100

    Footer fix

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 			
 			<blockquote>"Mindaz amit teszel, az a döntések halogatása. A korlátok teszik nagyszerűvé a zenét." - Matt Wallace</blockquote>
 			
-			 Jó, hogy itt vagy! <br> Az oldal 2015 októberben indult útjára. <br>
+			 Jó, hogy itt vagy! <br> Az oldal 2015 októberben indult útjára. 
 			 Ez már a második változat: 2016 február óta.
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
