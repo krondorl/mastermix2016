666e0760afe150a5cbfb89686d32ccdc581f1769
Adam Burucs <adam.burucs@gmail.com>
2016-10-18 22:44:04 +0200

    Add custom 404 page

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="error-404 not-found">
+				<header class="page-header">
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<h1>Upsz! Ez az oldal nem létezik.</h1>
+					<p>Kérlek nézz körül inkább a <a href="/blog/">Blogon</a> vagy a <a href="/">Főoldalon</a>!</p>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+
+		</main><!-- .site-main -->
+
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
