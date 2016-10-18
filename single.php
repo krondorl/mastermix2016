88810a3e0bbad779da960e0539f4941f2e88379a
Adam Burucs <adam.burucs@gmail.com>
2016-10-18 21:56:42 +0200

    Blogposzt CTA hozzaadas es Footer stilus kicsinyit

--- single.php
+++ single.php
@@ -45,5 +45,9 @@
 	<?php get_sidebar( 'content-bottom' ); ?>
 
 </div><!-- .content-area -->
-
+<div class="want-more">
+  <h3>Még többet szeretnél tudni?</h3>
+  <p>Töltsd le az ingyenes mixing és mastering könyvem!<br><small>(5000 Ft értékben)</small></p>
+  <a href="/" class="download">Letöltés</a>
+</div>
 <?php get_footer(); ?>
