0a72ee67f4f1a030e6613c078338e33719292127
Adam Burucs <adam.burucs@gmail.com>
2016-10-19 22:37:34 +0200

    Add Testimonials for Front Page

--- front-page.php
+++ front-page.php
@@ -2,129 +2,202 @@
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-	
-	 <h2>Könyv</h2>
-
-    <div class="clearfix">
+      <h2>Könyv</h2>
       <div class="clearfix">
-        <div class="pull-left col-xs-5 col-sm-3">
-          <img class="book" src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/mastering-mixing-konyv-borito.jpg" alt="Audio mastering és mixing könyv borító">  
-        </div>
-        <div class="">
-          <p>Átfogó könyv a mixingről és masteringről, hangmérnöki szempontból (final mix, mixdown, "végső keverés", "lekeverés").
-          <p>Töltsd le a 72 oldalas <strong>INGYENES</strong> könyvet (<em>5000 Ft értékben</em>) és tanuld meg, hogy készíts <em>jobb hangzást!</em></p>
-          <div>
-            <img class="download-arrow-down" src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/nyil-lefele.svg" alt="">
+        <div class="clearfix">
+          <div class="pull-left col-xs-5 col-sm-3">
+            <img class="book" src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/mastering-mixing-konyv-borito.jpg" alt="Audio mastering és mixing könyv borító">
           </div>
-          <div style="border: 2px solid orange; border-radius: 4px;">
-            <?php include_once 'subscribe.php'; ?>
-          </div>        
+          <div class="">
+            <p>Átfogó könyv a mixingről és masteringről, hangmérnöki szempontból (final mix, mixdown, "végső keverés", "lekeverés").
+            <p>Töltsd le a 72 oldalas <strong>INGYENES</strong> könyvet (<em>5000 Ft értékben</em>) és tanuld meg, hogy készíts <em>jobb hangzást!</em></p>
+            <div>
+              <img class="download-arrow-down" src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/nyil-lefele.svg" alt="">
+            </div>
+            <div style="border: 2px solid orange; border-radius: 4px;">
+              <?php include_once 'subscribe.php'; ?>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-   <h2 class="toc-h2">Tartalomjegyzék</h2>
-   
-   <div class="clearfix toc">
-    <ul class="col-sm-12">
-      <li>Bevezetés</li>
-      <li>Hardver</li>
-      <li>Szoftver</li>
-      <li>Előkészületek</li>
-      <li>Hangerő és panoráma (volume, panning)</li>
-      <li>Dinamika (compressor, limiter, ...)</li>
-      <li>Hangszínszabályzás (EQ, equalizer)</li>
-      <li>Visszhang (delay)</li>
-      <li>Zengető (reverb)</li>
-      <li>Modulációs effektek (phaser, flanger, chorus)</li>
-      <li>Torzító (overdrive, distortion, fuzz)</li>
-      <li>Szaturáció (saturation)</li>
-      <li>Sztereó vastagítás (stereo widening)</li>
-      <li>Fázis (phase)</li>
-      <li>Tranziensek (transient)</li>
-      <li>Mid/side feldolgozás</li>
-      <li>Sidechain működés</li>
-      <li>Automatizálás</li>
-      <li>Vizuális eszközök</li>
-      <li>Audio formátumok</li>
-      <li>Emberi tényezők a keverés során</li>
-      <li>Stílusoktól független keverés</li>
-      <li>Korrektív és kreatív folyamat</li>
-      <li>Referencia zenék használata</li>
-      <li>Imitáció</li>
-      <li>Első mixing példa</li>
-      <li>Második mixing példa</li>
-      <li>Harmadik mixing példa</li>
-      <li>Negyedik mixing példa</li>
-      <li>Ötödik mixing példa</li>
-      <li>Hatodik mixing példa</li>
-      <li>CPU erőforrás menedzsment</li>
-      <li>Időmenedzsment</li>
-      <li>Együttműködés a mastering hangmérnökkel</li>
-      <li>Mastering - bevezetés</li>
-      <li>Mastering – dinamika</li>
-      <li>Mastering – EQ</li>
-      <li>Mastering – sztereó kép</li>
-      <li>Mastering – hangerő, limiter</li>
-      <li>Mastering – effektlánc</li>
-      <li>Mastering – munkafolyamat</li>
-      <li>Mastering példa</li>
-      <li>Hozzáállás és fejlődés</li>
-      <li>Művelődjünk!</li>
-      <li>További tananyagok</li>
-      <li>Mixing ellenőrzési lista</li>
-      <li>Mastering ellenőrzési lista</li>
-      <li>Elérhetőség</li>
+     <h2 class="toc-h2">Tartalomjegyzék</h2>
 
-    </ul>
-     
-   </div>
+     <div class="clearfix toc">
+      <ul class="col-sm-12">
+        <li>Bevezetés</li>
+        <li>Hardver</li>
+        <li>Szoftver</li>
+        <li>Előkészületek</li>
+        <li>Hangerő és panoráma (volume, panning)</li>
+        <li>Dinamika (compressor, limiter, ...)</li>
+        <li>Hangszínszabályzás (EQ, equalizer)</li>
+        <li>Visszhang (delay)</li>
+        <li>Zengető (reverb)</li>
+        <li>Modulációs effektek (phaser, flanger, chorus)</li>
+        <li>Torzító (overdrive, distortion, fuzz)</li>
+        <li>Szaturáció (saturation)</li>
+        <li>Sztereó vastagítás (stereo widening)</li>
+        <li>Fázis (phase)</li>
+        <li>Tranziensek (transient)</li>
+        <li>Mid/side feldolgozás</li>
+        <li>Sidechain működés</li>
+        <li>Automatizálás</li>
+        <li>Vizuális eszközök</li>
+        <li>Audio formátumok</li>
+        <li>Emberi tényezők a keverés során</li>
+        <li>Stílusoktól független keverés</li>
+        <li>Korrektív és kreatív folyamat</li>
+        <li>Referencia zenék használata</li>
+        <li>Imitáció</li>
+        <li>Első mixing példa</li>
+        <li>Második mixing példa</li>
+        <li>Harmadik mixing példa</li>
+        <li>Negyedik mixing példa</li>
+        <li>Ötödik mixing példa</li>
+        <li>Hatodik mixing példa</li>
+        <li>CPU erőforrás menedzsment</li>
+        <li>Időmenedzsment</li>
+        <li>Együttműködés a mastering hangmérnökkel</li>
+        <li>Mastering - bevezetés</li>
+        <li>Mastering – dinamika</li>
+        <li>Mastering – EQ</li>
+        <li>Mastering – sztereó kép</li>
+        <li>Mastering – hangerő, limiter</li>
+        <li>Mastering – effektlánc</li>
+        <li>Mastering – munkafolyamat</li>
+        <li>Mastering példa</li>
+        <li>Hozzáállás és fejlődés</li>
+        <li>Művelődjünk!</li>
+        <li>További tananyagok</li>
+        <li>Mixing ellenőrzési lista</li>
+        <li>Mastering ellenőrzési lista</li>
+        <li>Elérhetőség</li>
 
-	
-	 <h2 class="about">Rólunk</h2>
-	<div class="clearfix about-section">
-      <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/adam.jpg" alt="Burucs Ádám fotó">
-        <h3>Burucs Ádám</h3>
-        <h4>szerző, mixing, mastering, zene</h4>
-        <p>A könyv szerzője és az oldal alapítója. Karrierje zongoraleckékkel kezdődött, majd jött 2001-ben a <a href="http://adamfiregate.com/" target="_blank">zeneszerzés</a>, hangszín-tervezés és a <a href="https://www.facebook.com/afmastering/" target="_blank">hangmérnöki tevékenységek</a>. 2013 óta ír angol nyelvű <a href="http://tutsplus.com/authors/adam-burucs" target="_blank">szakmai cikkeket</a>.</p>
+      </ul>
+
+     </div>
+
+      <h2>Visszajelzések</h2>
+
+      <h3 class="checkmark">A könyvről</h3>
+      <figure>
+        <blockquote>
+        "Letisztult gondolatok, szerzői tapasztalatokra épített információk, melyek sallangmentes környezetet idézve elő, segítenek a tudásbővítésben. Külön kiemelném a DAW specifikus plugin-park leírás és magyarázat összeköttetését, ami véleményem szerint ritka jelenség egy szabad kiadványban."
+        </blockquote>
+        <footer>
+        — <cite class="author">Bencsik Gábor</cite>
+        </footer>
+      </figure>
+
+      <figure>
+        <blockquote>
+        "Nos, személyes véleményem szerint különösen hasznos volt, amikor hat mixing példán keresztül is bemutattad a tanultakat, ugyanis az elméletet sok helyen is látni / olvasni lehet, de konkrétan nem igazán láttam bemutatva, hogy na ezt ezért meg azért csináltam. Továbbá egy kezdő számára szerintem kifejezetten hasznos, hogy több apróbb tanácsot is megosztasz, pl. CPU erőforrások megtartása, időmenedzsment, illetve, hogy mi - milyen hatással lehet a fejlődésre."
+        </blockquote>
+        <footer>
+        — <cite class="author">Sándor Tamás</cite>
+        </footer>
+      </figure>
+
+      <figure>
+        <blockquote>
+        "Tisztelt Zeneszerkesztők! Nemrégiben hozzájutottam ehhez a nagyszerű anyaghoz itt a csoportban. Rengeteg dolgot lehet belőle tanulni. Érthetően, rávezetve dolgokra fogalmazza meg a témákat, az alapoktól egészen a végső fázisig Burucs Ádám kollégánk. Nekem kifejezetten tetszik a hangszínszabályozás témakör, sok érdekes dolgot tanultam belőle."
+        </blockquote>
+        <footer>
+        — <cite class="author">Balázs Róbert</cite>
+        </footer>
+      </figure>
+
+      <figure>
+        <blockquote>
+        "Csak mindig mással kellett csináltatni [a mixet], és így nem haladtunk. Igen, sokat segített. De ahhoz, hogy az elejetől a végéig gyakorlatban sikerüljön viszonylag profin, ahhoz más kell. De így az alapokba betekintést nyer az ember elég jó szinten. Respect érte!"
+        </blockquote>
+        <footer>
+        — <cite class="author">Takács Péter</cite>
+        </footer>
+      </figure>
+      <hr>
+      <h3 class="checkmark">Az Audio Technica füles cikkről</h3>
+
+      <figure>
+        <blockquote>
+        "Aranyat érő..."
+        </blockquote>
+        <footer>
+        — <cite class="author">Tóth Roland</cite>
+        </footer>
+      </figure>
+
+      <figure>
+        <blockquote>
+        "Köszönjük a cikket Ádám! Még csak szemezgettem (nem fülezgettem) a nyitott fülesekkel, ezért kimondottan érdekelt a különbség."
+        </blockquote>
+        <footer>
+        — <cite class="author">Mácsai Zoltán</cite>
+        </footer>
+      </figure>
+
+     <figure>
+        <blockquote>
+        "Bár több Hozzád hasonló tudású és tapasztalatú ember osztaná meg ilyen önzetlenül és értékesen másokkal a szellemi javait! Remek cikk, hatalmas tisztelet és köszönet érte! Tartsd meg ezt a jó szokásod!"
+        </blockquote>
+        <footer>
+        — <cite class="author">Mészáros Máté</cite>
+        </footer>
+      </figure>
+
+      <hr>
+      <h3 class="checkmark">A videókról</h3>
+
+      <figure>
+        <blockquote>
+        "Egyre jobbak ezek a videók, jó úton haladsz! :) Jah és thx a Mac-es [letöltő] linkért is!"
+        </blockquote>
+        <footer>
+        — <cite class="author">Chudák Róbert</cite>
+        </footer>
+      </figure>
+      <hr>
+      <h2 class="about">Rólunk</h2>
+      <div class="clearfix about-section">
+        <div class="col-md-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/adam.jpg" alt="Burucs Ádám fotó">
+          <h3>Burucs Ádám</h3>
+          <h4>szerző, mixing, mastering, zene</h4>
+          <p>A könyv szerzője és az oldal alapítója. Karrierje zongoraleckékkel kezdődött, majd jött 2001-ben a <a href="http://adamfiregate.com/" target="_blank">zeneszerzés</a>, hangszín-tervezés és a <a href="https://www.facebook.com/afmastering/" target="_blank">hangmérnöki tevékenységek</a>. 2013 óta ír angol nyelvű <a href="http://tutsplus.com/authors/adam-burucs" target="_blank">szakmai cikkeket</a>.</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/spet.jpg" alt="Szigetvári Zsolt fotó">
+          <h3>Szigetvári Zsolt</h3>
+          <h4>szakmai lektor, zenész</h4>
+          <p><a href="https://soundcloud.com/spetmusic" target="_blank">Londonban élő producer</a>, drum and bass és rap mester.</p>
+        </div>
+        <div class="col-md-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/deepsec.jpg" alt="Dobóczi Attila fotó">
+          <h3>Dobóczi Attila</h3>
+          <h4>szakmai lektor, zenész</h4>
+          <p><a href="https://soundcloud.com/deepsec" target="_blank">Deep és progi house felelős</a>. Mindig van valamilyen egyedi hangszerelési ötlete. Terek és atmoszférák megalkotója.</p>
+        </div>
+
       </div>
-      <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/spet.jpg" alt="Szigetvári Zsolt fotó">
-        <h3>Szigetvári Zsolt</h3>
-        <h4>szakmai lektor, zenész</h4>
-        <p><a href="https://soundcloud.com/spetmusic" target="_blank">Londonban élő producer</a>, drum and bass és rap mester.</p>
-      </div>
-      <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/deepsec.jpg" alt="Dobóczi Attila fotó">
-        <h3>Dobóczi Attila</h3>
-        <h4>szakmai lektor, zenész</h4>
-        <p><a href="https://soundcloud.com/deepsec" target="_blank">Deep és progi house felelős</a>. Mindig van valamilyen egyedi hangszerelési ötlete. Terek és atmoszférák megalkotója.</p>
-      </div>
-	  
-	</div>
-	
-	<h2>Cikkek</h2>
-	
-	<a href="/blog"><img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/cikkek.png" alt="Cikkek"></a>
-	
-	<h2>Videók</h2>
-	
-	<a href="https://www.youtube.com/channel/UCDG-_4npV8JescQZiBb1mUg" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/videok.jpg" alt="Videók"></a>
-	
-	<h2 id="kapcsolat">Kapcsolat</h2>
-	
-	<img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/contact-studio.jpg" alt="High end studio fotó">
-	
-	<p>
-      <a href="https://www.facebook.com/groups/audiomasteringesmixing/" target="_blank">Csatlakozz a FaceBook csoporthoz!</a><br>
-      <a href="https://www.facebook.com/afmastering/" target="_blank">Kérj ajánlatot fizetős mixing és mastering szolgáltatásra vagy zene kritikára!</a><br>
-	</p>
 
-	<div>
-	  <?php echo do_shortcode('[contact-form-7 id="4" title="1 kapcsolati űrlap"]'); ?>
-	</div>
-			
+      <h2>Cikkek</h2>
+
+      <a href="/blog"><img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/cikkek.png" alt="Cikkek"></a>
+
+      <h2>Videók</h2>
+
+      <a href="https://www.youtube.com/channel/UCDG-_4npV8JescQZiBb1mUg" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/videok.jpg" alt="Videók"></a>
+
+      <h2 id="kapcsolat">Kapcsolat</h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/contact-studio.jpg" alt="High end studio fotó">
+      <p>
+        <a href="https://www.facebook.com/groups/audiomasteringesmixing/" target="_blank">Csatlakozz a FaceBook csoporthoz!</a><br>
+        <a href="https://www.facebook.com/afmastering/" target="_blank">Kérj ajánlatot fizetős mixing és mastering szolgáltatásra vagy zene kritikára!</a><br>
+      </p>
+      <div>
+        <?php echo do_shortcode('[contact-form-7 id="4" title="1 kapcsolati űrlap"]'); ?>
+      </div>
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
 
