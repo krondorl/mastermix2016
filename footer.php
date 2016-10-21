39e6ded5aa17b86cb0d580cbb53b90d8f22c6c93
Adam Burucs <adam.burucs@gmail.com>
2016-10-21 21:09:47 +0200

    Add icon credits to Footer

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
 
 	</div><!-- .site-inner -->
 </div><!-- .site -->
-  
+
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
 			<a href="#top" class="jump-top">&uArr; Ugrás az oldal tetejére &uArr;</a>
 
 			<blockquote>"Mindaz amit teszel, az a döntések halogatása. A korlátok teszik nagyszerűvé a zenét." - Matt Wallace</blockquote>
 
-			 Ikonok: Dario Ferrando / flaticon.com
+			 Ikonok: Dario Ferrando, Gregor Cresnar, MadeByOliver, Freepik / flaticon.com
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 
