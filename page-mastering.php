14fa80d3dc3ccacf34e0fa1b078d7f99db06f5a8
Adam Burucs <adam.burucs@gmail.com>
2017-01-14 00:38:22 +0100

    Add Mastering Page

---
+++ page-mastering.php
@@ -0,0 +1,281 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+    <h1>Rám bíznád a zenéd véglegesítését?</h1>
+    <h2>Érdeklődj nálam.</h2>
+
+    <h2>Kezdd itt!</h2>
+    <p>Mielőtt konkrét anyagot küldenél hozzám, kérlek küldd át a zenédet egy előzetes meghallgatásra:</p>
+    <ul>
+    <li><code>adam.burucs@masteringmixing.hu</code></li>
+    <li>Soundcloud fetöltés</li>
+    <li>privát megosztás</li>
+    </ul>
+
+    <h2>Ügyféllista</h2>
+    <p>A több mint 150 mastering során többek között a következő művészek zenéin dolgoztam:</p>
+
+    <ul>
+    <li>Robbie Fithon - mixing, mastering, production</li>
+    <li>Indio - stem mastering</li>
+    <li>Cold Side Player - mixing, mastering</li>
+    <li>The 69 Project - mastering</li>
+    <li>Hollab - mixing, mastering, production</li>
+    <li>Thomas Szalkay - mixing, mastering</li>
+    <li>Technikai Atallas - mastering</li>
+    <li>Secdam Project - mixing, mastering, production</li>
+    <li>Display Girl / Pilotcamp - mixing, mastering</li>
+    <li>JCK - mastering</li>
+    <li>Cale Jera - mixing, mastering, production</li>
+    <li>Benjamin Witt - mixing, mastering, production</li>
+    <li>Deepsec - mixing, mastering, production</li>
+    <li>Frensys - mixing, mastering, production</li>
+    <li>Mr. Slack - mixing, mastering</li>
+    <li>Miklos Matrai - mixing, mastering, production</li>
+    <li>Tensile Force - mixing, mastering, production</li>
+    <li>Vortex Panda Entertainment - mixing, mastering, sound design</li>
+    <li>Rusty - mixing, mastering</li>
+    <li>Four Elements - mastering</li>
+    <li>Martin - mastering</li>
+    <li>Comstock - mixing, mastering, production</li>
+    <li>Stranger - mastering</li>
+    <li>Incarnation - mixing, mastering</li>
+    <li>Eva Rambala - mixing, mastering, editing</li>
+    </ul>
+
+    <h2>A többiek véleménye</h2>
+
+    <figure>
+      <blockquote>
+      "The mix overall is punchy and warm, mostly due to the nice drums and respectable guitars. I like the HiHats on this mix. It could be the EQ-ing or the reverb on them. But I definitely took notice."
+      </blockquote>
+      <footer>
+      — <cite class="author">H-man / Ben Hehir</cite>
+      </footer>
+    </figure>
+
+    <figure>
+      <blockquote>
+      "Érezhetően és hallhatóan gyönyörű! Nagyon ügyes vagy!"
+      </blockquote>
+      <footer>
+      — <cite class="author">Cale ID / Cale Jera</cite>
+      </footer>
+    </figure>
+
+   <figure>
+      <blockquote>
+      "Nem a mi stílusunk. Viszont a Master! Az nagyon jó!"
+      </blockquote>
+      <footer>
+      — <cite class="author">Rimoshee Production (kiadó és terjesztő cég ügyvezetője)</cite>
+      </footer>
+    </figure>
+
+    <figure>
+      <blockquote>
+      "Tökéletes. Remekmű lett, köszönöm!"
+      </blockquote>
+      <footer>
+      — <cite class="author">Deepsec</cite>
+      </footer>
+    </figure>
+
+   <figure>
+      <blockquote>
+      "Király. Köszi! Szép munka. Azt hiszem, ha mastering kell, hozzád fordulok bizalommal!"
+      </blockquote>
+      <footer>
+      — <cite class="author">Indio</cite>
+      </footer>
+    </figure>
+
+    <figure>
+      <blockquote>
+      "Tökéletes lett.  Nagyon jó. Örök hálám!"
+      </blockquote>
+      <footer>
+      — <cite class="author">Mr Slack</cite>
+      </footer>
+    </figure>
+
+
+    <h2>Stem Mastering csomagok</h2>
+    <table>
+    <thead>
+    <tr>
+    <th>Alap csomag</th>
+    <th>Producer csomag</th>
+    <th>Mester csomag</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td>12.000 Ft</td>
+    <td>15.000 Ft</td>
+    <td>22.000 Ft</td>
+    </tr>
+    <tr>
+    <td>tanácsadás</td>
+    <td>tanácsadás</td>
+    <td>tanácsadás</td>
+    </tr>
+    <tr>
+    <td>1 zene</td>
+    <td>1 zene</td>
+    <td>1 zene</td>
+    </tr>
+    <tr>
+    <td>4 sávig</td>
+    <td>8 sávig</td>
+    <td>12 sávig</td>
+    </tr>
+    <tr>
+    <td>5 munkanapon belül</td>
+    <td>3 munkanapon belül</td>
+    <td>2 munkanapon belül</td>
+    </tr>
+    <tr>
+    <td>100% egyedi hang, sablon nélkül</td>
+    <td>100% egyedi hang, sablon nélkül</td>
+    <td>100% egyedi hang, sablon nélkül</td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>részletes leírás a fejlődéshez</td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>VIP tagság 1 hónapra</td>
+    </tr>
+    </tbody>
+    </table>
+    <h2>Mixing + Mastering csomagok</h2>
+    <table>
+    <thead>
+    <tr>
+    <th>Alap csomag</th>
+    <th>Producer csomag</th>
+    <th>Mester csomag</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td>32.000 Ft</td>
+    <td>45.000 Ft</td>
+    <td>55.000 Ft</td>
+    </tr>
+    <tr>
+    <td>tanácsadás</td>
+    <td>tanácsadás</td>
+    <td>tanácsadás</td>
+    </tr>
+    <tr>
+    <td>1 zene</td>
+    <td>1 zene</td>
+    <td>1 zene</td>
+    </tr>
+    <tr>
+    <td>30 sávig</td>
+    <td>60 sávig</td>
+    <td>korlátlan sávig</td>
+    </tr>
+    <tr>
+    <td>7 munkanapon belül</td>
+    <td>3 munkanapon belül</td>
+    <td>2 munkanapon belül</td>
+    </tr>
+    <tr>
+    <td>100% egyedi hang, sablon nélkül</td>
+    <td>100% egyedi hang, sablon nélkül</td>
+    <td>100% egyedi hang, sablon nélkül</td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>részletes leírás a fejlődéshez</td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>VIP tagság 1 hónapra</td>
+    </tr>
+    </tbody>
+    </table>
+    <h2>Projekt file</h2>
+    <p>Ezzel a kész mixing projektet odaadom. Tökéletes tanulásra, inspirációra. Belelátsz mit-hogy csináltam.</p>
+    <p><strong>Ára: +8.000 Ft</strong> felár a teljes összeghez.</p>
+    <p>Részletek:</p>
+    <ul>
+      <li>formátum: Ableton Suite 9</li>
+      <li>pluginek: kizárólag Ableton pluginek</li>
+      <li>audio: WAV</li>
+    </ul>
+    <h2>Számla</h2>
+    <p>Az árak nettóban értendők, adómentes (ÁFA mentes) számlát tudok kiállítani róluk.</p>
+    <h2>VIP tagság</h2>
+    <p>A következőket tartalmazza:</p>
+    <table>
+    <tbody>
+    <tr>
+    <td>időtartam</td>
+    <td>a vásárlás napjától 1 hónapig érvényes</td>
+    </tr>
+    <tr>
+    <td>elsőbbség</td>
+    <td>időben előbb kerülsz sorra (előzetes egyeztetés alapján)</td>
+    </tr>
+    <tr>
+    <td>megoldás</td>
+    <td>bővebb tanácsadás</td>
+    </tr>
+    <tr>
+    <td>segítség</td>
+    <td>konzultáció közvetlen telefonon</td>
+    </tr>
+    <tr>
+    <td>fejlődés</td>
+    <td>részletes cselekvési terv</td>
+    </tr>
+    </tbody>
+    </table>
+    <h2>Konzultáció</h2>
+    <p>Csak személyesen veled foglalkozom 60 percen keresztül.</p>
+    <p>Jelenleg csak előzetes bejelentkezéssel és indokolt esetben fogadok új tanítványt. Kérlek Legalább 2-3 héttel előre jelezz.</p>
+    <p><strong>10.000 Ft / 60 perc</strong></p>
+    <p>Telefonon és voice chaten elérhető vagyok. Személyes találkozás kizárólag nálam lehetséges Budapesten XI. kerületben.</p>
+    <h2>Beküldés</h2>
+    <p>Kérlek tartsd be az alábbiakat:</p>
+    <ul>
+    <li>Master sáv legyen Teljesen ÜRES</li>
+    <li>megengedett csúcs hangerő: maximum -6 dBFS</li>
+    <li>kikapcsolt Dither</li>
+    <li>formátum: 44.1 kHz, 24 bit, WAV</li>
+    <li>használj ZIP tömörítést</li>
+    <li>elnevezési sablon: <code>eloadonev-szamcim__valami_remix__savok.zip</code> </li>
+    </ul>
+    <p>Stem sávok bontása (4 db esetén):</p>
+    <ul>
+    <li>dobok / <code>drums.wav</code></li>
+    <li>basszus / <code>bass.wav</code></li>
+    <li>közép / <code>mids.wav</code></li>
+    <li>vokál + effektek / <code>fx.wav</code></li>
+    </ul>
+    <p>Fetöltés módja:</p>
+    <ul>
+    <li>Dropbox</li>
+    <li>Google Drive</li>
+    <li>WeTransfer</li>
+    </ul>
+    <p>Ide küldd a csomagot: <code>adam.burucs@masteringmixing.hu</code></p>
+
+    <h3>— Köszi az érdeklődést! Jó zenélést. üdv, Ádám</h3>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
