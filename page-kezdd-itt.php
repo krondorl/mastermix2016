04abfd5a85b35d17ab8ba2e864044f29c9f5420c
Adam Burucs <adam.burucs@gmail.com>
2017-01-14 10:02:33 +0100

    Add Start Here page

---
+++ page-kezdd-itt.php
@@ -0,0 +1,67 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+
+<h1>3 dolog, amiben a MasteringMixing más, mint hasonló anyagok:</h1>
+
+<figure>
+  <blockquote>
+  "Az igazi művészek szállítanak."
+  </blockquote>
+  <footer>
+  — <cite>Steve Jobs</cite>
+  </footer>
+</figure>
+
+<p>Steve itt arra gondol, hogy folyamatosan eredményeket tesznek le az asztalra. Mellébeszélés nélkül.</p>
+
+<ol>
+<li>Én ténylegesen csinálom is azokat a dolgokat, amikről beszélek. <strong>11 év aktív zenélés után</strong> mertem leírni az első cikkemet.</li>
+<li>Nem árulok varázspirulát. A lényeg a cselekvés. Csak akkor lesznek eredményeid, ha cselekszel.</li>
+<li>A <strong>gyakorlatra</strong> helyezem a hangsúlyt. Mert ez szállítja az eredeményeket. Nem fogok neked előadást tartani a hangok fizikájából. Nem fogunk differenciál egyenleteket megoldani a hangszínszabályzás beállításához. <br> Nem fogunk lineáris algebrát tanulni. És nem fogunk Fourier transzformációkat sem végezni.</li>
+</ol>
+
+<h2>Üdv! Burucs Ádám vagyok.</h2>
+
+<p>
+  <img src="<?php echo get_template_directory_uri(); ?>/../mastermix2016/img/adam.jpg" alt="Burucs Ádám fotó">
+</p>
+
+<p>Évekig csak a zeneszerzés és készítés érdekelt. Azonban egy idő után ez kevés volt. Szerettem volna, ha a zenéim jobban szólnának.</p>
+
+<figure>
+  <blockquote>
+  "Minél kreatívabban dolgozom az életemen, annál szerencsésebb vagyok."
+  </blockquote>
+</figure>
+
+<p>Mivel pénzem nem volt, ezért úgy döntöttem megtanulom magamnak a hangmérnöki gyakorlatot. Minden eszközt megragadtam, amit találtam. Rengeteg külföldi könyv, videó, és gyakorlás után végül megtaláltam a megoldást. Azóta elérhetővé vált számomra a nemzetközi, versenyképes hangzás.</p>
+
+
+<h2>Mi ez a MasteringMixing?</h2>
+
+<p>Ebben a projektben adom át azt a tudást, amit az utóbbi 15 évben szereztem.</p>
+<p>A 15 év lehet sok is. Vagy kevés. Azt figyeltem meg, hogy ez nem feltétlen a tudásszintet jelenti. <strong>Hanem az ELKÖTELEZŐDÉST.</strong></p>
+
+<figure>
+  <blockquote>
+  "Nem mindenki akar hangmérnök lenni."
+  </blockquote>
+</figure>
+
+<p>Ezzel egyetértek. De sokan szeretnének profibb hangzást. Akár az alapokat szeretnéd tudni, akár többet, tudok benne segíteni.</p>
+
+<div style="text-align: center;">
+  <h1>Vágj bele!</h1>
+  <h2>Töltsd le az ingyenes Mastering és Mixing könyvem (5000 Ft értékben)!</h2>
+  <h2>Már több mint 1500 ember tanult ebből a könyvből.</h2>
+  <h3>Cselekedj. Gyakorolj. Ismételj!</h3>
+  <a href="/" class="download">Letöltés</a>
+</div>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
