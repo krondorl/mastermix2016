1145d4c681e2f0454ed180207fde9394aca0c265
Adam Burucs <adam.burucs@gmail.com>
2017-01-20 18:09:33 +0100

    Add Footer Links

--- footer.php
+++ footer.php
@@ -5,11 +5,13 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
-			<a href="#top" class="jump-top">&uArr; Ugrás az oldal tetejére &uArr;</a>
-
-			<blockquote>"Mindaz amit teszel, az a döntések halogatása. A korlátok teszik nagyszerűvé a zenét." - Matt Wallace</blockquote>
-
-			 Ikonok: Dario Ferrando, Gregor Cresnar, MadeByOliver, Freepik / flaticon.com
+			<p><a href="https://www.facebook.com/groups/audiomasteringesmixing/" target="_blank">Facebook</a> | <a href="https://www.youtube.com/channel/UCDG-_4npV8JescQZiBb1mUg/featured" target="_blank">Youtube</a> |
+			<a href="https://www.facebook.com/afmastering/" target="_blank">AF Mastering</a> |
+			<a href="/adatvedelmi-tajekoztato">Adatvédelmi Tájékoztató</a> | <a href="/felelossegi-nyilatkozat">Felelősségi Nyilatkozat</a></p>
+			<p>
+			  <a href="#top" class="jump-top">&uArr; Ugrás az oldal tetejére &uArr;</a>
+			</p>
+			 <p>Ikonok: Dario Ferrando, Gregor Cresnar, MadeByOliver, Freepik / flaticon.com</p>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 
