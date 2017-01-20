82fd5c9a50d190251c80bb5ed322fde2c86d3d5a
Adam Burucs <adam.burucs@gmail.com>
2017-01-20 18:09:15 +0100

    Add Privacy Doc

---
+++ page-adatvedelmi-tajekoztato.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+    <h1>Adatvédelmi Tájékoztató</h1>
+   <p>(MasteringMixing: ez a weboldal, www.masteringmixing.hu)</p>
+    <p>A MasteringMixing online tevékenységével kapcsolatban kezeli a regisztrált felhasználók személyes adatait. Csak olyan személyes adatot kezel, amely az adatkezelés céljának megvalósulásához elengedhetetlen, és a cél elérésére alkalmas.</p>
+    <p>A személyes adatokat csak a cél megvalósulásához szükséges mértékben és ideig kezeli. Az Adatkezelő minden elvárható módon védi a regisztrált felhasználóknak a kezelt személyes adatait. </p>
+    
+    <p>Az Adatkezelő adatai</p>
+    <p><strong>Név:</strong> Burucs Ádám egyéni vállalkozó<br />
+    <strong>Cím:</strong> 1113 Budapest, Tardoskedd u. 7.<br />
+    <strong>Adószám:</strong> 78777718-1-43 <br />
+    <strong>Telefon:</strong> +36-70-381-9794<br />
+    <strong>Email:</strong> adam@masteringmixing.hu </p>
+    
+    <p>Az Adatkezelő által használt Szolgáltató</p>
+    <p>
+      <strong>Név:</strong> ActiveCampaign, LLC <br />
+      <strong>Webcím:</strong> <a href="http://www.activecampaign.com/">www.activecampaign.com</a> <br />
+      <strong>Telefon:</strong> +1 (773) 904-0945 <br />
+      <strong>Email:</strong> info@activecampaign.com
+    </p>
+    
+    <p>Az adatkezelés jogalapja: 2011. évi CXII. törvény az információs önrendelkezési jogról és az információszabadságról 5. § (1) bekezdés a) Az érintett hozzájárulása. </p>
+    <p>Hozzájárulás az adatkezeléshez: A felasználó regisztrációnál kifejezetten hozzájárulását adja az általa önkéntesen megadott személyes adatainak kezeléséhez. </p>
+    <p><strong>Az adatgyűjtés célja:</strong> tájékoztatás, oktatás, letölthető anyagok, termékajánlás és kapcsolattartás. </p>
+    <p><strong>Az adatkezelés időtartama</strong> a leiratkozásig tart, amit a felhasználó intéz. </p>
+    <p>A felhasználó kérelmezheti az adatkezelőnél </p>
+    <ul>
+    <li>tájékoztatását személyes adatai kezeléséről, </li>
+    <li>személyes adatainak helyesbítését, </li>
+    <li>személyes adatainak törlését vagy zárolását. </li>
+    </ul>
+    <p>Az adatkezelő a kérelem benyújtásától számított legrövidebb idő alatt, legfeljebb azonban 10 napon belül, közérthető formában, a regisztrált felhasználó erre irányuló kérelmére írásban megadja a tájékoztatást. Ha a regisztrált felhasználó kérelmét jogosnak találja, akkor haladéktalanul intézkedik a személyes adatainak helyesbítése vagy törlése érdekében. </p>
+    <p>A 2011. évi CXII. törvény az információs önrendelkezési jogról és az információszabadságról itt olvasható: <a href="http://www.njt.hu/">Nemzeti Jogszabálytár</a>.</p>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
