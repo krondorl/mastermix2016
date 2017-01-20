96938b894e31c4122cdca07a111dd48036328ae7
Adam Burucs <adam.burucs@gmail.com>
2017-01-20 18:09:07 +0100

    Add Liability Doc

---
+++ page-felelossegi-nyilatkozat.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	 <h1>Felelősségi Nyilatkozat</h1>
+
+      <p>(Készítő: a weboldal készítője, Burucs Ádám.)</p>
+      <p>(MasteringMixing: ez a weboldal, www.masteringmixing.hu)</p>
+      <p>A MasteringMixing weboldal célja oktatási anyagok közzététele. A Készítő mindent megtesz azért, hogy a felé jelzett hibákat kijavíta. Mindemellett a Készítő semmilyen felelősséget vagy kötelezettséget nem vállal az oldalon megjelent információkért.</p>
+      <p>Ezek az információk:</p>
+      <ul>
+      <li>általános jellegűek,</li>
+      <li>nem feltétlenül teljes, átfogó, pontos vagy naprakész információk,</li>
+      <li>időnként olyan külső honlapok felé irányíthatnak, amelyek felett a Készítő nem rendelkezik ellenőrzési joggal, és amelyeket illetően semmiféle felelősséget nem vállal;</li>
+      <li>nem minősülnek szakmai tanácsadásnak.</li>
+      </ul>
+      <p>A Készítő mindent elkövet, hogy csökkentse a technikai hibákból eredő kellemetlenségeket.</p>
+      <p>A Készítő a MasteringMixing használatából vagy más, e honlap által javasolt külső honlap használatából eredő problémákért semmilyen felelősséget nem vállal.</p>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
