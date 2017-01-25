fabc542703b182bee1c444f02a7db8352181bb3f
Adam Burucs <adam.burucs@gmail.com>
2017-01-25 18:38:50 +0100

    Add Products page

---
+++ page-termekek.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	 <h1>Termékek</h1>
+
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
