a1db5ba68f9289c687bf17722693065d0e200613
Adam Burucs <adam.burucs@gmail.com>
2017-02-10 21:15:09 +0100

    Refactor Styles

--- functions.php
+++ functions.php
@@ -3,13 +3,11 @@
   add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
   function theme_enqueue_styles() {
-      wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), rand(111,9999), 'all' );
   }
 
   // Override font loading, with empty function.
   // No webfonts needed!
   function twentysixteen_fonts_url() {
-
   }
 
 function disable_wp_emojicons() {
