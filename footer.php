ea9359888f4f34597e3f97dcc6a4e1c47e25908c
Adam Burucs <adam.burucs@gmail.com>
2017-09-13 12:33:34 +0200

    Update Footer text

--- footer.php
+++ footer.php
@@ -8,10 +8,11 @@
 			<p><a href="https://www.facebook.com/groups/audiomasteringesmixing/" target="_blank">Facebook</a> | <a href="https://www.youtube.com/channel/UCDG-_4npV8JescQZiBb1mUg/featured" target="_blank">Youtube</a> |
 			<a href="https://www.facebook.com/afmastering/" target="_blank">AF Mastering</a> |
 			<a href="/adatvedelmi-tajekoztato/">Adatvédelmi Tájékoztató</a> | <a href="/felelossegi-nyilatkozat/">Felelősségi Nyilatkozat</a></p>
-			<p>
+			<p class="p-footer-jump">
 			  <a href="#top" class="jump-top">&uArr; Ugrás az oldal tetejére &uArr;</a>
 			</p>
-			 <p>Ikonok: Google, SimpleIcon, Dario Ferrando, Gregor Cresnar, MadeByOliver, Freepik / flaticon.com</p>
+			 <p class="p-footer-icons">Ikonok: Google, SimpleIcon, Dario Ferrando, Gregor Cresnar, MadeByOliver, Freepik / flaticon.com</p>
+			 <p class="p-footer-copyright">&copy; 2015-<?php echo date("Y"); ?> Burucs Ádám e.v.</p>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 
